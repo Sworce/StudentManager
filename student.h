43b0caeeeef22905689dd24af690491736006be3
Sworce <Sworce>
2019-12-04 16:08:42 -0800

    added student.h file

---
+++ student.h
@@ -0,0 +1,16 @@
+#ifndef _STUDENT_H_
+#define _STUDENT_H_
+
+class student 
+{
+    private:
+            string fname;
+            string lname;
+
+    public:
+            void setName(string, string);
+            string fullName();
+
+};
+
+#endif
